5166309a6a13b8e95e18fbff85db2ea0769df90d
kimhyuk <kimhyuk722@live.co.kr>
2022-05-05 07:26:17 +0900

    add guessing_game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+extern crate rand;
+use rand::prelude::*;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(0..101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(v) => v,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
+}
