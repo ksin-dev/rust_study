b0c5a319c35abd00b5c1974dbdfbf7f23ac42cf3
kimhyuk <kimhyuk722@live.co.kr>
2022-07-08 19:31:27 +0900

    variables 추가

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+
+#![allow(unused)]
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+
+    x = 6;
+
+    println!("The value of x is: {}", x);
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+}
+
+
