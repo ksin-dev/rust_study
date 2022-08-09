ca2bc9728425727753efad74a9957d07c0e8d2c0
kimhyuk <kimhyuk722@live.co.kr>
2022-08-10 01:23:10 +0900

    update match pattern

--- enum_and_pattern/src/main.rs
+++ enum_and_pattern/src/main.rs
@@ -6,9 +6,30 @@ enum IpAddr {
 }
 
 fn main() {
+
+    // enum
     let v4 = IpAddr::V4(String::from("192.168.2.1"));
     let v6 = IpAddr::V6(String::from("::1"));
 
 
     println!("v4: {:?} , v6: {:?}", v4,v6);
+
+
+    //match
+    match v4 {
+        IpAddr::V4(_) => println!("true"),
+        IpAddr::V6(_) => println!("false"),
+    }
+
+    let i = Some(1);
+
+    let new_i = if let Some(1) = i {
+        1
+    } else {
+        0
+    };
+
+    println!("{}",new_i);
+
+    
 }
